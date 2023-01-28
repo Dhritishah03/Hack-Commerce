45b9dd90182bde3368cace9a3ec86ab1be3499c5
gauriket <gauriket@gmail.com>
2023-01-28 14:39:02 +0530

    signup

---
+++ cart/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { ShoppingCart} from './cart';
+
+
+function App() {
+  return (
+    <div className="container">
+      <ShoppingCart/>
+    </div>
+  );
+}
+
+export default App;
--- login/src/Register.jsx
+++ login/src/Register.jsx
@@ -4,6 +4,8 @@ export const SignUp = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [name, setName] = useState('');
+    const [phoneno, setPhoneno] = useState('');
+    const [address, setAddress] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -20,7 +22,13 @@ export const SignUp = (props) => {
             <label htmlFor="email">email</label>
             <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
             <label htmlFor="password">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="***********" id="password" name="password" />
+            <label htmlFor="password">confirm password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="***********" id="password" name="password" />
+            <label htmlFor="password"> password</label>
+            <input value={phoneno} onChange={(e) => setPhoneno(e.target.value)}type="phoneno" placeholder="+91-XXXXXXXXXX" id="phoneno" name="phoneno" />
+            <label htmlFor="password">Phone No</label>
+            <input value={address} onChange={(e) => setAddress(e.target.value)} type="address" placeholder=" " id="address" name="address" />
             <button type="submit">Sign Up</button>
         </form>
         <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
